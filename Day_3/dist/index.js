d84c5f7b48a5cb51979015a7c78ef8133f410f7b
Anonymous <Anonymous@gmail.com>
2025-07-14 00:02:01 +0700

    Initial commit: add Day_1 to Day_4 folders

---
+++ Day_3/dist/index.js
@@ -0,0 +1,18 @@
+/*
+function sayHello(name: string) {
+  console.log("Hello " + name);
+}
+
+sayHello("World");
+
+import { name } from "./lib.js";
+
+sayHello(name);
+*/
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dir = fs.readdirSync(__dirname);
+console.log(dir);
---
+++ Day_4/typescript-nextjs/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Card from "@components/Card";
+
+export default function Home() {
+  return (
+    <div>
+      Hello world
+      <Card title="Card Title 1" text="GG" />
+      <Card title="Card Title 2" text="GG" />
+      <Card title="Card Title 3" text="GG" />
+      <Card title="Card Title 4" text="GG" />
+    </div>
+  );
+}
